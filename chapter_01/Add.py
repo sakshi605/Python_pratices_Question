dd55caf95bead4b693047d629363a781728e1bea
Sakshi Priya <128884815+sakshi605@users.noreply.github.com>
2025-07-23 10:42:00 +0000

    Add_Number

---
+++ chapter_01/Add.py
@@ -0,0 +1,5 @@
+# Corrected Python code to add two numbers
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+sum_of_numbers = first_number + second_number
+print("Sum of the two numbers:", sum_of_numbers)
